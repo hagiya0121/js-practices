42d9ebb534966910d05b18ba81071c665901931b
hagiya0121 <koazu0121@gmail.com>
2024-07-16 18:39:09 +0900

    refactor: 変数宣言をletからconstに変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -19,7 +19,7 @@ console.log("日 月 火 水 木 金 土");
 
 process.stdout.write(" ".repeat(date.day() * 3));
 while (date.month() === args.month - 1) {
-  let day = date.format("D").padStart(2);
+  const day = date.format("D").padStart(2);
   process.stdout.write(day + " ");
   date.day() === 6 && console.log();
   date = date.add(1, "day");
