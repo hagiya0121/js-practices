49df5eae206e309aa7675c1a347423eb42e3c45b
hagiya0121 <koazu0121@gmail.com>
2024-07-23 17:06:14 +0900

    feat: DBからメモ一覧を取得

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -2,38 +2,62 @@
 
 import readline from "readline";
 import sqlite3 from "sqlite3";
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let lines = [];
-rl.on("line", (input) => {
-  lines.push(input);
-});
-
-rl.on("close", () => {
-  const memo = lines.join("\n");
-  const db = new MemoDatabase();
-  db.createTable(() => {
-    db.add(memo);
-  });
-});
+import minimist from "minimist";
+import { promisify } from "util";
 
 class MemoDatabase {
   constructor() {
     this.db = new sqlite3.Database("./db.sqlite");
+    this.run = promisify(this.db.run.bind(this.db));
+    this.all = promisify(this.db.all.bind(this.db));
   }
 
-  createTable(callback) {
-    this.db.run("CREATE TABLE IF NOT EXISTS memos (content TEXT)", () => {
-      callback();
-    });
+  async createTable() {
+    await this.run("CREATE TABLE IF NOT EXISTS memos (content TEXT)");
   }
 
-  add(memo) {
+  async add(memo) {
     const statement = this.db.prepare("INSERT INTO memos VALUES (?)");
     statement.run(memo).finalize();
   }
+
+  async list() {
+    return await this.all("SELECT content FROM memos");
+  }
 }
+
+class MemoApp {
+  constructor(args) {
+    this.args = args;
+  }
+
+  async main() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    const db = new MemoDatabase();
+
+    if (args.l) {
+      const memos = await db.list();
+      console.log(memos);
+    } else {
+      let lines = [];
+      rl.on("line", (input) => {
+        lines.push(input);
+      });
+
+      rl.on("close", async () => {
+        const memo = lines.join("\n");
+        await db.createTable();
+        db.add(memo);
+      });
+    }
+  }
+}
+const options = {
+  boolean: ["l"],
+};
+const args = minimist(process.argv.slice(2), options);
+const memoApp = new MemoApp(args);
+memoApp.main();
