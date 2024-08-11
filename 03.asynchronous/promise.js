17586a71e4a4aa2e4e7680903e7d988017774557
hagiya0121 <koazu0121@gmail.com>
2024-08-11 20:36:08 +0900

    feat: promise エラーあり

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,4 +1,5 @@
 import sqlite3 from "sqlite3";
+import timers from "timers/promises";
 
 const db = new sqlite3.Database(":memory:");
 
@@ -36,5 +37,42 @@ db.run_promise(
   })
   .then((row) => {
     console.log(row.title);
+    db.run("DROP TABLE books");
+  });
+
+await timers.setTimeout(100);
+
+db.run_promise = function (query, params = []) {
+  return new Promise((resolve, reject) => {
+    db.run(query, params, function (err) {
+      err && reject(err);
+      resolve(this);
+    });
+  });
+};
+
+db.get_promise = function (query, params = []) {
+  return new Promise((resolve, reject) => {
+    db.get(query, params, function (err, row) {
+      err && reject(err);
+      resolve(row);
+    });
+  });
+};
+
+db.run_promise(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => {
+    return db.run_promise("INSERT INTO books (title) VALUES (?)", null);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .then(() => {
+    return db.get_promise("SELECT tittle FROM books WHERE id = ?", 1);
+  })
+  .catch((err) => {
+    console.log(err);
     db.run("DROP TABLE books");
   });
