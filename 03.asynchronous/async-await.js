ca6fe28b63497dcf7e0cd48a376d3acd0b1a7043
hagiya0121 <koazu0121@gmail.com>
2024-08-12 21:09:08 +0900

    feat: async/await エラーあり

--- 03.asynchronous/async-await.js
+++ 03.asynchronous/async-await.js
@@ -1,4 +1,5 @@
 import sqlite3 from "sqlite3";
+import timers from "timers/promises";
 
 const db = new sqlite3.Database(":memory:");
 
@@ -36,3 +37,46 @@ async function run() {
   db.run("DROP TABLE books");
 }
 run();
+
+await timers.setTimeout(100);
+
+// エラーあり
+db.run_promise = function (query, params = []) {
+  return new Promise((resolve, reject) => {
+    db.run(query, params, function (err) {
+      err && reject(err);
+      resolve(this);
+    });
+  });
+};
+
+db.get_promise = function (query, params = []) {
+  return new Promise((resolve, reject) => {
+    db.get(query, params, function (err, row) {
+      err && reject(err);
+      resolve(row);
+    });
+  });
+};
+
+async function run2() {
+  try {
+    await db.run_promise(
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    );
+    await db.run_promise("INSERT INTO books (title) VALUES (?)", null);
+  } catch (err) {
+    if (err.code === "SQLITE_CONSTRAINT") {
+      console.log(err);
+    }
+  }
+  try {
+    await db.get_promise("SELECT tittle FROM books WHERE id = ?", 1);
+  } catch (err) {
+    if (err.code === "SQLITE_ERROR") {
+      console.log(err);
+    }
+  }
+  db.run("DROP TABLE books");
+}
+run2();
