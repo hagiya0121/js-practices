1d27cb878ce702f4b3558b7770d588647c833ebb
hagiya0121 <koazu0121@gmail.com>
2024-08-16 18:46:16 +0900

    refactor: 不要な関数を削除

--- 03.asynchronous/async-await.js
+++ 03.asynchronous/async-await.js
@@ -4,48 +4,45 @@ import { runPromise, getPromise } from "./db-promise.js";
 const db = new sqlite3.Database(":memory:");
 
 // エラーなし
-async function main() {
+await runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+const result = await runPromise(
+  db,
+  "INSERT INTO books (title) VALUES (?)",
+  "New Record",
+);
+console.log(result.lastID);
+const row = await getPromise(
+  db,
+  "SELECT title FROM books WHERE id = ?",
+  result.lastID,
+);
+console.log(row.title);
+await runPromise(db, "DROP TABLE books");
+
+// エラーあり
+try {
   await runPromise(
     db,
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   );
-  const result = await runPromise(
-    db,
-    "INSERT INTO books (title) VALUES (?)",
-    "New Record",
-  );
-  console.log(result.lastID);
-  const row = await getPromise(
-    db,
-    "SELECT title FROM books WHERE id = ?",
-    result.lastID,
-  );
-  console.log(row.title);
-  await runPromise(db, "DROP TABLE books");
-
-  // エラーあり
-  try {
-    await runPromise(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
-    await runPromise(db, "INSERT INTO books (title) VALUES (?)", null);
-  } catch (err) {
-    if (err?.code === "SQLITE_CONSTRAINT") {
-      console.error(err.message);
-    } else {
-      throw err;
-    }
+  await runPromise(db, "INSERT INTO books (title) VALUES (?)", null);
+} catch (err) {
+  if (err?.code === "SQLITE_CONSTRAINT") {
+    console.error(err.message);
+  } else {
+    throw err;
   }
-  try {
-    await getPromise(db, "SELECT tittle FROM books WHERE id = ?", 1);
-  } catch (err) {
-    if (err?.code === "SQLITE_ERROR") {
-      console.error(err.message);
-    } else {
-      throw err;
-    }
-  }
-  await runPromise(db, "DROP TABLE books");
 }
-main();
+try {
+  await getPromise(db, "SELECT tittle FROM books WHERE id = ?", 1);
+} catch (err) {
+  if (err?.code === "SQLITE_ERROR") {
+    console.error(err.message);
+  } else {
+    throw err;
+  }
+}
+await runPromise(db, "DROP TABLE books");
