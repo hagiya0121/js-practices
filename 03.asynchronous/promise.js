0d846209558f4b0edf66df0c3cd1896390abba1e
hagiya0121 <koazu0121@gmail.com>
2024-08-17 19:46:56 +0900

    refactor: promise版のエラーありとエラーなしを統合

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,3 +1,4 @@
+import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 import { runPromise, getPromise } from "./db-promise.js";
 
@@ -21,5 +22,22 @@ runPromise(
   })
   .then((row) => {
     console.log(row.title);
+    return runPromise(db, "DROP TABLE books");
+  });
+
+await timers.setTimeout(100);
+
+// エラーあり
+runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => runPromise(db, "INSERT INTO books (title) VALUES (?)", null))
+  .catch((err) => {
+    console.error(err.message);
+    return getPromise(db, "SELECT tittle FROM books WHERE id = ?", 1);
+  })
+  .catch((err) => {
+    console.error(err.message);
     return runPromise(db, "DROP TABLE books");
   });
